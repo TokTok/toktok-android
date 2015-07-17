297d503222d8ef4153d85866dd5680e0bd9048f8
andrealmieda <andrealmieda@gmail.com>
2015-07-17 19:32:01 +0000

    Changed the friends list item layout

--- src/main/scala/im/tox/toktok/app/MainActivity/MainFriendsFragment/FriendsRecyclerAdapter.scala
+++ src/main/scala/im/tox/toktok/app/MainActivity/MainFriendsFragment/FriendsRecyclerAdapter.scala
@@ -3,8 +3,10 @@ package im.tox.toktok.app.MainActivity.MainFriendsFragment
 import android.content.{Intent, Context}
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.util.{Log}
+import android.view.View.OnClickListener
 import android.view.{LayoutInflater, View, ViewGroup}
-import android.widget.{LinearLayout, TextView}
+import android.widget.{ImageButton, LinearLayout, TextView}
 import com.github.siyamed.shapeimageview.CircularImageView
 import im.tox.toktok.R
 import im.tox.toktok.app.ContactsActivity.ContactsActivity
@@ -15,16 +17,18 @@ import scala.collection.mutable.ListBuffer
 class FriendsRecyclerAdapter(list: ListBuffer[Friend]) extends RecyclerView.Adapter[FriendsRecyclerViewHolder] {
 
   private val items: ListBuffer[Friend] = list
+  private var active : View = null;
 
   def onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): FriendsRecyclerViewHolder = {
     val itemView: View = LayoutInflater.from(viewGroup.getContext).inflate(R.layout.fragment_home_friends_item, viewGroup, false)
-    return new FriendsRecyclerViewHolder(itemView,list)
+    return new FriendsRecyclerViewHolder(itemView,list,active)
   }
 
   def onBindViewHolder(viewHolder: FriendsRecyclerViewHolder, position: Int) = {
     val item: Friend = items(position)
     viewHolder.mUserName.setText(item.getUserName())
     viewHolder.mUserImage.setImageResource(item.getPhotoReference())
+
   }
 
   def getItemCount(): Int = {
@@ -37,7 +41,7 @@ class FriendsRecyclerAdapter(list: ListBuffer[Friend]) extends RecyclerView.Adap
 
 }
 
-final class FriendsRecyclerViewHolder(itemView: View, list: ListBuffer[Friend]) extends RecyclerView.ViewHolder(itemView) with View.OnClickListener {
+class FriendsRecyclerViewHolder(itemView: View, list: ListBuffer[Friend],active : View) extends RecyclerView.ViewHolder(itemView) with View.OnClickListener {
 
   var context: Context = itemView.getContext
 
@@ -46,6 +50,37 @@ final class FriendsRecyclerViewHolder(itemView: View, list: ListBuffer[Friend]) 
   var mLayout: LinearLayout = itemView.findViewById(R.id.home_friends_layout).asInstanceOf[LinearLayout]
   //var mUserStatus: TextView = itemView.findViewById(R.id.home_friends_status).asInstanceOf[TextView]
   var mUserImage: CircularImageView = itemView.findViewById(R.id.home_friends_img).asInstanceOf[CircularImageView]
+  var mExpandButton : ImageButton = itemView.findViewById(R.id.home_friends_expend).asInstanceOf[ImageButton]
+  var mCallButton = itemView.findViewById(R.id.home_friends_call)
+  var mMessageButton = itemView.findViewById(R.id.home_friends_message)
+
+
+  mCallButton.setOnClickListener(new OnClickListener {
+    override def onClick(v: View): Unit = {
+      Log.d("Tox","Call")
+    }
+  })
+
+
+  mMessageButton.setOnClickListener(new OnClickListener {
+    override def onClick(v: View): Unit = {
+      Log.d("Tox","Message")
+    }
+  })
+
+
+  mExpandButton.setOnClickListener(new OnClickListener {
+    override def onClick(v: View): Unit ={
+
+      v.setVisibility(View.INVISIBLE)
+      itemView.setElevation(5)
+      itemView.setBackground(itemView.getResources.getDrawable(R.drawable.cardboard_ripple))
+      itemView.findViewById(R.id.home_friends_expended_area).setVisibility(View.VISIBLE)
+
+    }
+
+
+  })
 
 
   def onClick(view: View) = {
