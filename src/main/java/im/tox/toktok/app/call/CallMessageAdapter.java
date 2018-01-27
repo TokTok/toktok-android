5ad0bb2e852d9a1b83511323ca08f2aa9648f6be
iphydf <iphydf@users.noreply.github.com>
2018-01-27 13:02:26 +0000

    Migrate Call*Adapter to Java.

---
+++ src/main/java/im/tox/toktok/app/call/CallMessageAdapter.java
@@ -0,0 +1,50 @@
+package im.tox.toktok.app.call;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import im.tox.toktok.R;
+
+final class CallMessageAdapter extends RecyclerView.Adapter<CallMessageViewHolder> {
+
+    private final List<String> excuses;
+
+    CallMessageAdapter(List<String> excuses) {
+        this.excuses = excuses;
+    }
+
+    @Override
+    public CallMessageViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        LinearLayout itemView = (LinearLayout) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.call_item, viewGroup, false);
+        return new CallMessageViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CallMessageViewHolder viewHolder, int position) {
+        String item = excuses.get(position);
+        viewHolder.mMessage.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return excuses.size();
+    }
+
+}
+
+final class CallMessageViewHolder extends RecyclerView.ViewHolder {
+
+    final TextView mMessage;
+
+    CallMessageViewHolder(LinearLayout itemView) {
+        super(itemView);
+        this.mMessage = itemView.findViewById(R.id.call_item_message);
+    }
+
+}
---
+++ src/main/java/im/tox/toktok/app/call/CallOnGoingContactsAdapter.java
@@ -0,0 +1,57 @@
+package im.tox.toktok.app.call;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import im.tox.toktok.R;
+import im.tox.toktok.app.domain.Friend;
+
+final class CallOnGoingContactsAdapter extends RecyclerView.Adapter<CallOnGoingContactsViewHolder> {
+
+    private final List<Friend> friends;
+
+    CallOnGoingContactsAdapter(List<Friend> friends) {
+        this.friends = friends;
+    }
+
+    @Override
+    public CallOnGoingContactsViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        RelativeLayout itemView = (RelativeLayout) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.call_ongoing_contact, viewGroup, false);
+        return new CallOnGoingContactsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CallOnGoingContactsViewHolder viewHolder, int position) {
+        Friend item = friends.get(position);
+        viewHolder.mFriendName.setText(item.userName());
+        viewHolder.mFriendImage.setImageResource(item.photoReference());
+    }
+
+    @Override
+    public int getItemCount() {
+        return friends.size();
+    }
+
+}
+
+final class CallOnGoingContactsViewHolder extends RecyclerView.ViewHolder {
+
+    final CircleImageView mFriendImage;
+    final TextView mFriendName;
+    final TextView mFriendCallTime;
+
+    CallOnGoingContactsViewHolder(RelativeLayout itemView) {
+        super(itemView);
+        mFriendImage = itemView.findViewById(R.id.call_ongoing_contact_img);
+        mFriendName = itemView.findViewById(R.id.call_ongoing_contact_name);
+        mFriendCallTime = itemView.findViewById(R.id.call_ongoing_contact_calltime);
+    }
+
+}
