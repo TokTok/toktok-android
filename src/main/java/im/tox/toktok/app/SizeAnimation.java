7bb8cda95698dace2ca41480d9433b60aeccda2a
iphydf <iphydf@users.noreply.github.com>
2018-01-27 11:27:44 +0000

    Migrate SizeAnimation to Java.

---
+++ src/main/java/im/tox/toktok/app/SizeAnimation.java
@@ -0,0 +1,42 @@
+package im.tox.toktok.app;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public final class SizeAnimation extends Animation {
+
+    private final View view;
+    private final int oldSize;
+    private final int newSize;
+    private final int sizeChange;
+
+    public SizeAnimation(View view, int newSize, int sizeChange) {
+        this.view = view;
+        this.oldSize = sizeChange == 0 ? view.getWidth() : view.getHeight();
+        this.newSize = newSize;
+        this.sizeChange = sizeChange;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        final int currentSize = (int) (oldSize + ((newSize - oldSize) * interpolatedTime));
+
+        switch (sizeChange) {
+            case 0:
+                view.getLayoutParams().width = currentSize;
+                break;
+            case 1:
+                view.getLayoutParams().height = currentSize;
+                break;
+        }
+
+        view.requestLayout();
+    }
+
+    @Override
+    public boolean willChangeBounds() {
+        return true;
+    }
+
+}
